9cae54bf231b8172ab94a4595f58b37a1b874439
muhammed-jahas <jahaspty1@gmail.com>
2024-02-14 13:33:19 +0530

    fix: code improvement

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_midjourney_app/features/prompt/presentation/create_prompt_screen.dart';
 import 'package:flutter_midjourney_app/features/prompt/presentation/splash_screen.dart';
 import 'package:flutter_midjourney_app/resources/app_colors.dart';
-void main(){
+
+void main() {
   runApp(const MyApp());
 }
 
@@ -12,32 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // darkTheme:ThemeData(
-      // primaryColor: const Color(0xFFFF3D3D),
-      //   primaryColorDark: const Color(0xFFFF3D3D), 
-      //   // buttonTheme: ButtonThemeData(
-      //   //   buttonColor: const Color(0xFFFF3D3D),
-          
-      //   // ),
-      //   appBarTheme:AppBarTheme(
-      //     backgroundColor: Colors.grey.shade900,
-      //     elevation: 0
-      //   ) ,
-      //   brightness: Brightness.dark,
-      //   scaffoldBackgroundColor: Colors.grey.shade900,
-      // ),
-      theme:ThemeData(
-       fontFamily: 'oldburg',
-      // primaryColor: const Color(0xFFFF3D3D),
-      //   primaryColorDark: const Color(0xFFFF3D3D), 
-        
-        appBarTheme:AppBarTheme(
-          backgroundColor: AppColors.appBarColor1,
-          elevation: 0
-        ) ,
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: AppColors.scaffoldBackgroundColor
-      ),
+      theme: ThemeData(
+          fontFamily: 'oldburg',
+          // primaryColor: const Color(0xFFFF3D3D),
+          //   primaryColorDark: const Color(0xFFFF3D3D),
+
+          appBarTheme: AppBarTheme(
+              backgroundColor: AppColors.appBarColor1, elevation: 0),
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: AppColors.scaffoldBackgroundColor),
       home: SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
