6dd4169bb1580da56eabef524312381cbdcc095b
muhammed-jahas <jahaspty1@gmail.com>
2024-03-14 16:25:16 +0530

    fix: change launcher icon

--- lib/features/prompt/repos/prompt_repo.dart
+++ lib/features/prompt/repos/prompt_repo.dart
@@ -1,48 +1,45 @@
-import 'dart:developer';
-import 'dart:io';
-
 import 'dart:typed_data';
-
 import 'package:dio/dio.dart';
 
-class PromptRepo{
+class PromptRepo {
+  // ignore: body_might_complete_normally_nullable
   static Future<Uint8List?> generateImage(String prompt) async {
     try {
-      String url  = 'https://api.vyro.ai/v1/imagine/api/generations';
-    Map<String, dynamic> headers = {
-       'Authorization': 'Bearer vk-FrNI3pMzSJdOn20Hr68Rm19mLIDgX32kGRHRHTHvGkQYN4'
-    };
-    Map<String,dynamic> payload = {
-      'prompt' : prompt,
-      'style_id' : '122',
-      'aspec_ratio' : '1:1',
-      'cfg' : '1',
-      'seed' : '1',
-      'high_res_results' : '1'
-    };
+      String url = 'https://api.vyro.ai/v1/imagine/api/generations';
+      Map<String, dynamic> headers = {
+        'Authorization':
+            'Bearer vk-ujoF1rdY2FHVi2GsxD3ODC9J8nYsxVXOV2FvfUh8BQc7OLyl'
+      };
+      print(prompt);
+      Map<String, dynamic> payload = {
+        'prompt': prompt,
+        'style_id': '122',
+        'aspec_ratio': '1:1',
+        'cfg': '3',
+        'seed': '1',
+        'high_res_results': '1'
+      };
 
-    FormData formData = FormData.fromMap(payload);
+      FormData formData = FormData.fromMap(payload);
 
-    Dio dio = Dio();
-    dio.options = BaseOptions(
-      headers: headers,
-      responseType: ResponseType.bytes
-    );
-    final response = await dio.post(url,data: formData, );
+      Dio dio = Dio();
+      dio.options =
+          BaseOptions(headers: headers, responseType: ResponseType.bytes);
+      final response = await dio.post(
+        url,
+        data: formData,
+      );
 
-    if(response.statusCode == 200 ){
-      // log(response.data.runtimeType.toString());
-      // log(response.data.toString());
-      Uint8List uint8list = Uint8List.fromList(response.data);
-      return uint8list;
-    }else{
-      return null;
-    }
+      if (response.statusCode == 200) {
+        // log(response.data.runtimeType.toString());
+        // log(response.data.toString());
+        Uint8List uint8list = Uint8List.fromList(response.data);
+        return uint8list;
+      } else {
+        return null;
+      }
     } catch (e) {
-     print(e);
+      print(e);
     }
-
   }
 }
-
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_midjourney_app/features/prompt/presentation/create_prompt_screen.dart';
 import 'package:flutter_midjourney_app/features/prompt/presentation/splash_screen.dart';
 import 'package:flutter_midjourney_app/resources/app_colors.dart';
 
@@ -15,14 +14,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
           fontFamily: 'oldburg',
-          // primaryColor: const Color(0xFFFF3D3D),
-          //   primaryColorDark: const Color(0xFFFF3D3D),
-
-          appBarTheme: AppBarTheme(
-              backgroundColor: AppColors.appBarColor1, elevation: 0),
+          appBarTheme: const AppBarTheme(
+              backgroundColor:  AppColors.appBarColor1, elevation: 0),
           brightness: Brightness.dark,
           scaffoldBackgroundColor: AppColors.scaffoldBackgroundColor),
-      home: SplashScreen(),
+          
+      home: const SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
